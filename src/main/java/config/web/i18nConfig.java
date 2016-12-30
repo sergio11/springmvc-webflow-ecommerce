be696ecd3f2e993e9f486ba6f3f1388fccf10318
Sergio Sánchez <sss4esob@gmail.com>
2016-12-30 12:55:50 +0100

    feat(frontend): product detail page

--- src/main/java/config/web/i18nConfig.java
+++ src/main/java/config/web/i18nConfig.java
@@ -1,10 +1,12 @@
 package config.web;
 
 import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -14,11 +16,14 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
  */
 @Configuration
 public class i18nConfig {
+    
+    @Autowired
+    private Environment env;
 
     @Bean(name="messageSource")
     public MessageSource  provideMessageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.addBasenames("/WEB-INF/i18n/messages");
+        messageSource.addBasenames(env.getProperty("i18n.admin.messages.basename"), env.getProperty("i18n.frontend.messages.basename"));
         messageSource.setFallbackToSystemLocale(true);
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
