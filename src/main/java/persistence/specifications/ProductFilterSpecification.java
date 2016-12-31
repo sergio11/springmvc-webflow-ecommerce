bbedf492dfe9ba6f6d3aeb3c7fae0e4b268e27d1
Sergio Sánchez <sss4esob@gmail.com>
2016-12-31 21:16:27 +0100

    feat(admin): improve product filter

--- src/main/java/persistence/specifications/ProductFilterSpecification.java
+++ src/main/java/persistence/specifications/ProductFilterSpecification.java
@@ -26,13 +26,25 @@ public class ProductFilterSpecification implements Specification<Product>{
     @Override
     public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
         List<Predicate> predicates = new ArrayList<>();
-        // filter for product price from
+        // filter by product id
+        if(filterProduct.getId() != null){
+            predicates.add(cb.equal(root.get(Product_.id), filterProduct.getId()));
+        }
+        // filter by product price from
         if(filterProduct.getPriceFrom() != null){
             predicates.add(cb.greaterThanOrEqualTo(root.get(Product_.price), filterProduct.getPriceFrom()));
         }
-        // filter for product price to
+        // filter by product price to
         if(filterProduct.getPriceTo() != null){
             predicates.add(cb.lessThanOrEqualTo(root.get(Product_.price), filterProduct.getPriceTo()));
+        }
+        // filter by name
+        if(filterProduct.getName() != null && !filterProduct.getName().isEmpty()){
+            predicates.add(cb.like(root.get(Product_.name), "%"+filterProduct.getName()+"%"));
+        }
+        // filter by status
+        if(filterProduct.getStatus() != null){
+            predicates.add(cb.equal(root.get(Product_.status), filterProduct.getStatus()));
         }
         return andTogether(predicates, cb);
     }
