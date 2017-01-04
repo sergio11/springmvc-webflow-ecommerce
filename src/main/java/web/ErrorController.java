738281d65e40f3fac242854d3af4e96f3499a1d8
Sergio Sánchez <sss4esob@gmail.com>
2017-01-04 13:31:39 +0100

    refactor(admin): refactor ErrorControllerAdvice

--- src/main/java/web/ErrorController.java
+++ src/main/java/web/ErrorController.java
@@ -1,8 +1,11 @@
 package web;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,9 +22,19 @@ public class ErrorController {
     
     private static Logger logger = LoggerFactory.getLogger(ErrorController.class);
     
+    @Autowired
+    private HttpServletRequest request;
+    
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String invalidData(final ConstraintViolationException ex){
+        String referer = request.getHeader("Referer");
+        return "redirect:"+referer;
+    }
+    
     @ExceptionHandler(NotFoundException.class)
     public String notfound(final NotFoundException notFoundException, final Model model) {
-        return "global/errors/404";
+        return "redirect:global/errors/404";
     }
 
     @ExceptionHandler(Throwable.class)
@@ -31,6 +44,6 @@ public class ErrorController {
         throwable.printStackTrace();
         String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
         model.addAttribute("errorMessage", errorMessage);
-        return "global/errors/500";
+        return "redirect:global/errors/500";
     }
 }
