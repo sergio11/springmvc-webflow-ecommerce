af66b5d4ffb4d383ed2ec182076e7aa738b3bea0
Sergio Sánchez Sánchez <sss4esob@gmail.com>
2017-02-08 10:08:57 +0100

    feat(admin): add SessionConnectInterceptor to websocket configuration

--- src/main/java/config/web/websocket/WebSocketConfig.java
+++ src/main/java/config/web/websocket/WebSocketConfig.java
@@ -1,10 +1,14 @@
 package config.web.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+import web.interceptors.SessionConnectInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,6 +23,16 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/order/new").withSockJS();
+	}
+	
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.setInterceptors(provideSessionConnectInterceptor());
+	}
+
+	@Bean
+	public SessionConnectInterceptor provideSessionConnectInterceptor() {
+		return new SessionConnectInterceptor();
 	}
 
 }
---
+++ src/main/java/web/interceptors/SessionConnectInterceptor.java
@@ -0,0 +1,33 @@
+package web.interceptors;
+
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+
+public class SessionConnectInterceptor extends ChannelInterceptorAdapter {
+	
+	private static Logger logger = LoggerFactory.getLogger(SessionConnectInterceptor.class);
+
+	@Override
+	public Message<?> preSend(Message<?> message, MessageChannel channel) {
+		MessageHeaders headers = message.getHeaders();
+	    SimpMessageType type = (SimpMessageType) headers.get("simpMessageType");
+	    String simpSessionId = (String) headers.get("simpSessionId");
+	    if (type == SimpMessageType.CONNECT) {
+	        Principal principal = (Principal) headers.get("simpUser");
+	        logger.debug("WsSession " + simpSessionId + " is connected for user " + principal.getName());
+	    } else if (type == SimpMessageType.DISCONNECT) {
+	    	logger.debug("WsSession " + simpSessionId + " is disconnected");
+	    }
+	    return message;
+	}
+
+	
+	
+}
